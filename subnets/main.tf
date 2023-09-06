a3e696f0ceb83a2ea486d0aab644492c405cf243
manoj <gandhamanojkutty@gmail.com>
2023-09-06 21:51:29 +0530

    route tables added

--- subnets/main.tf
+++ subnets/main.tf
@@ -15,4 +15,3 @@ resource "aws_route_table_association" "association" {
   count             = length(var.cidr_block)
   subnet_id      = aws_subnet.main[count.index].id
   route_table_id = aws_route_table.main[count.index].id
-}
