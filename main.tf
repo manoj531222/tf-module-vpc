85731014c509ef3bd17344456d963f1a53b81a3b
manoj <gandhamanojkutty@gmail.com>
2023-09-06 15:44:21 +0530

    subnets added

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_eip" "ngw" {
-  count = length(lookup(lookup(var.subnets, "public", null ), "cidr_block", 0))
+  count = length(var.subnets["public"].cidr_block)
   domain   = "vpc"
   tags = merge(var.tags, { Name = "${var.env}-ngw" })
 }
