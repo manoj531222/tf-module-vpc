823a6cdb4427e35afc86d6f117be0fbb940faf82
manoj <gandhamanojkutty@gmail.com>
2023-09-06 15:01:20 +0530

    subnets added

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
-  enable_dns_support = true
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = merge(var.tags, { name="${var.env}-vpc" })
-
+  tags                 = merge(var.tags, { Name = "${var.env}-vpc" })
 }
 
 module "subnets" {
@@ -13,7 +12,7 @@ module "subnets" {
   vpc_id     = aws_vpc.main.id
   cidr_block = each.value["cidr_block"]
   name       = each.value["name"]
-  azs = each.value["azs"]
+  azs        = each.value["azs"]
 
   tags = var.tags
   env  = var.env
@@ -22,5 +21,5 @@ module "subnets" {
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
-  tags = merge(var.tags, { name="${var.env}-igw" })
+  tags = merge(var.tags, { Name = "${var.env}-igw" })
 }
