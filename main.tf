01b766880059ee287defca7cee2ca25442c8cca1
manoj <gandhamanojkutty@gmail.com>
2023-09-06 12:25:07 +0530

    subnets added

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "aws_vpc" "main" {
   tags = merge(var.tags, {name="${var.env}-vpc"})
 
 }
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
