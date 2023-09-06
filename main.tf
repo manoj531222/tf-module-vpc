aa6145176e3e383fb9c4882ed246d48cfbe2c19c
manoj <gandhamanojkutty@gmail.com>
2023-09-06 14:46:52 +0530

    subnets added

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ module "subnets" {
   env  = var.env
 }
 
-resource "aws_internet_gateway" "Igw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = merge(var.tags, {name="${var.env}-igw"})
